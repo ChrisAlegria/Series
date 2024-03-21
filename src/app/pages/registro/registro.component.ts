d61ce4c87d759ee53c1a33a1a1a878798e33ecdb
«ChrisAlegria» <christian.alegriar@gmail.com>
2024-03-20 23:09:51 -0600

    Feat: Cancel Edition /  Confirm Mesages

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -16,6 +16,10 @@ export class RegistroComponent implements OnInit {
   selectedSerie: string = '';
   showUpdateButton: boolean = false;
   selectedSerieId: string | null = null; // Propiedad para almacenar el ID del registro seleccionado
+  hideDeleteButton: boolean = false;
+  mostrarBotonRojo: boolean = false;
+  mostrarBotonVerde: boolean = false; 
+
 
   constructor(private registroService: RegistroService, private agregarModificarEliminarService: AgregarModificarEliminarService) {}
 
@@ -34,16 +38,45 @@ export class RegistroComponent implements OnInit {
   }
 
   insertarRegistro() {
+    const confirmacion = confirm('¿Estás seguro de que deseas registrar esta serie?');
+    if (confirmacion) {
     this.agregarModificarEliminarService.createRegistro(this.registro);
     this.registro = new Registro();
+    }
   };
 
-  updateRegistro(){
-    this.agregarModificarEliminarService.updateRegistro(this.registro);
-    this.registro=new Registro();
+  updateRegistro() {
+    const confirmacion = confirm('¿Estás seguro de que deseas actualizar esta serie?');
+    if (confirmacion) {
+      this.agregarModificarEliminarService.updateRegistro(this.registro);
+      this.registro = new Registro();
+      this.selectedSerieId = null;
+      this.showUpdateButton = false; // Oculta el botón azul al presionar el botón azul
+      this.mostrarBotonRojo = true; // Muestra el botón rojo al presionar el botón azul
+    }
   }
 
+  cancelarEdicion() {
+    const confirmacion = confirm('¿Estás seguro de que deseas cancelar la edicion?');
+    if (confirmacion) {
+    this.showUpdateButton = false;
+    this.mostrarBotonVerde = false; // Se oculta el botón verde al cancelar la edición
+    this.mostrarBotonRojo = true;
+    this.registro = new Registro();
+    this.selectedSerieId = null;
+    }
+  }
 
+  deleteRegistro(id: string) {
+    const confirmacion = confirm('¿Estás seguro de que deseas eliminar esta serie?');
+    if (confirmacion) {
+      this.agregarModificarEliminarService.deleteRegistro(id);
+      this.registro = new Registro();
+      this.selectedSerieId = null;
+      this.showUpdateButton = false; // Oculta el botón azul al eliminar la serie
+      this.mostrarBotonRojo = true; // Muestra el botón rojo al presionar el botón morado
+    }
+}
 
   selectSerie() {
     if (this.selectedSerieId) {
@@ -57,18 +90,4 @@ export class RegistroComponent implements OnInit {
       this.registro = new Registro();
     }
   }
-  
-  selectRegistro(registroSeleccionado: Registro) {
-    this.registro = registroSeleccionado;
-    this.selectedSerieId = registroSeleccionado.id;
-  }
-  
-  deleteRegistro(id: string) {
-    const confirmacion = confirm('¿Estás seguro de que deseas eliminar esta serie?');
-    if (confirmacion) {
-      this.agregarModificarEliminarService.deleteRegistro(id);
-      this.registro = new Registro();
-      this.selectedSerieId = null;
-    }
-  }
-}
+} 
